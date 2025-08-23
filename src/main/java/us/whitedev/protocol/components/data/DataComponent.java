dd0a2eb56777269d19619be519eeb56c3eacb56f
Joyel99Dev <164185653+McpeDX@users.noreply.github.com>
2025-08-23 22:26:19 +0530

    Add files via upload

---
+++ src/main/java/us/whitedev/protocol/components/data/DataComponent.java
@@ -0,0 +1,10 @@
+package us.whitedev.protocol.components.data;
+
+import io.netty.buffer.ByteBuf;
+
+import java.util.Map;
+
+public interface DataComponent {
+    Map<Integer, Integer> getIds();
+    void write(ByteBuf buf);
+}
