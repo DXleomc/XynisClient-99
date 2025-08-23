af0cb049cf1c3c07ff56a1cd123e01564c698e91
Joyel99Dev <164185653+McpeDX@users.noreply.github.com>
2025-08-23 22:27:28 +0530

    Create ArmorMaterial.java

---
+++ src/main/java/us/whitedev/protocol/components/objects/ArmorMaterial.java
@@ -0,0 +1,8 @@
+package us.whitedev.protocol.components.objects;
+
+import net.minecraft.nbt.CompoundTag;
+
+import java.util.Map;
+
+public record ArmorMaterial(String assetName, int ingredient, Map<String, String> armorMaterials, CompoundTag tag) {
+}
