1cf1a1b2ba13d5a97f38fb9fc4e1dbb83ebbe87a
Joyel99Dev <164185653+McpeDX@users.noreply.github.com>
2025-08-24 13:14:52 +0530

    Add files via upload

---
+++ src/main/java/us/whitedev/protocol/XynisProtocol.java
@@ -0,0 +1,18 @@
+package us.whitedev.protocol;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraft.network.protocol.game.ServerboundKeepAlivePacket;
+import us.whitedev.helpers.PacketHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class XynisProtocol {
+    public static final List<ByteBuf> OUT_PACKETS = new ArrayList<>();
+    public static final List<Integer> SUPPORTED_PROTOCOLS = List.of(766, 767, 768, 769);
+
+    public void addPacketsToSend(List<ByteBuf> packets){
+        OUT_PACKETS.addAll(packets);
+        PacketHelper.send(new ServerboundKeepAlivePacket(0)); // only for trigger
+    }
+}
